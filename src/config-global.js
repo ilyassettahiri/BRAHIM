95472a8a34e239016e8ebd69cc8fcdc0dcd57ab2
Ilyaas <ilyaas@192.168.1.109>
2025-04-19 12:22:48 +0100

    232EDS45

--- src/config-global.js
+++ src/config-global.js
@@ -3,7 +3,7 @@ import packageJson from '../package.json';
 // ----------------------------------------------------------------------
 
 export const CONFIG = {
-  appName: 'Zone UI',
+  appName: 'Valcene',
   appVersion: packageJson.version,
   assetsDir: process.env.NEXT_PUBLIC_ASSETS_DIR ?? '',
   googleMapApiKey: process.env.NEXT_PUBLIC_MAP_API ?? '',
